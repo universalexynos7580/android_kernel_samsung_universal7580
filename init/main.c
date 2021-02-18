07fa7f74763e024908a607b196f6b62e0d3185a5
TALU <taluemail272@gmail.com>
2021-02-18 13:14:31 +0000

    init: remove support for deferred initcalls
    Signed-off-by: TALU <taluemail272@gmail.com>

--- init/main.c
+++ init/main.c
@@ -819,34 +819,6 @@ static int run_init_process(const char *init_filename)
 		(const char __user *const __user *)envp_init);
 }
 
-#ifdef CONFIG_DEFERRED_INITCALLS
-extern initcall_t __deferred_initcall_start[], __deferred_initcall_end[];
-
-/* call deferred init routines */
-void __ref do_deferred_initcalls(void)
-{
-	initcall_t *call;
-	static int already_run=0;
-
-	if (already_run) {
-		printk("do_deferred_initcalls() has already run\n");
-		return;
-	}
-
-	already_run=1;
-
-	printk("Running do_deferred_initcalls()\n");
-
-	for(call = __deferred_initcall_start;
-		call < __deferred_initcall_end; call++)
-		do_one_initcall(*call);
-
-	flush_scheduled_work();
-
-	free_initmem();
-}
-#endif
-
 #ifdef CONFIG_SEC_GPIO_DVS
 extern void gpio_dvs_check_initgpio(void);
 #endif
@@ -864,9 +836,7 @@ static int __ref kernel_init(void *unused)
 #endif
 	/* need to finish all async __init code before freeing the memory */
 	async_synchronize_full();
-#ifndef CONFIG_DEFERRED_INITCALLS
 	free_initmem();
-#endif
 	mark_rodata_ro();
 	system_state = SYSTEM_RUNNING;
 	numa_default_policy();
